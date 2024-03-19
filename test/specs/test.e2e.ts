7da9c0531f998d57421df7f53c3945783855ec81
muhserks <91063753+muhserks@users.noreply.github.com>
2024-03-19 13:56:14 +0100

    toHaveElementClass

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,11 +1,27 @@
 import { expect, browser, $ } from '@wdio/globals'
 
 describe('Test', () => {
-    it('toHaveText + expect.stringContaining', async () => {
-        await browser.url("C:\\work\\wdio\\test.html")
-        await expect($('//div[@name="a2"]')).toHaveText(expect.stringContaining("test"))
-    })
-    it('toHaveValue + expect.stringContaining', async () => {
-        await expect($('//input[@name="a1"]')).toHaveValue(expect.stringContaining("test"))
-    })
+    it('Single class', async () => {
+        await browser.url("C:\\work\\wdio\\test.html");
+        const div = await $('//div[@id="d1"]');
+        await expect(div).toHaveElementClass('clickable');
+        await expect(div).toHaveElementClass('step');
+        await expect(div).toHaveElementClass(expect.stringContaining('clickable'));
+        await expect(div).toHaveElementClass(expect.stringContaining('step'));
+    });
+
+    it('Multiplice classes 1', async () => {
+        const div = await $('//div[@id="d1"]');
+        await expect(div).toHaveElementClass('clickable step');
+    });
+
+    it('Multiplice classes 2', async () => {
+        const div = await $('//div[@id="d1"]');
+        await expect(div).toHaveElementClass(expect.stringContaining('clickable step'));
+    });
+
+    it('Multiplice classes 3', async () => {
+        const div = await $('//div[@id="d1"]');
+        await expect(div).toHaveElementClassContaining('clickable step');
+    });
 })
